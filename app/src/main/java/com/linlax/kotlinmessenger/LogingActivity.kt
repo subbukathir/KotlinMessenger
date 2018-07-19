15de8cb09ab55684fdea44064fb1c72f7ac4a81d
SubbuKathir <subbu.kathir123@gmail.com>
2018-07-19 19:36:48 +0530

    added firebase auth

--- app/src/main/java/com/linlax/kotlinmessenger/LogingActivity.kt
+++ app/src/main/java/com/linlax/kotlinmessenger/LogingActivity.kt
@@ -2,11 +2,17 @@ package com.linlax.kotlinmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_loging.*
 
 class LogingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loging)
+
+        tvBackToRegister.setOnClickListener {
+            finish()
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
     }
 }
--- app/src/main/java/com/linlax/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/linlax/kotlinmessenger/RegisterActivity.kt
@@ -3,13 +3,36 @@ package com.linlax.kotlinmessenger
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    val TAG = "RegisterActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val firebaseAuth = FirebaseAuth.getInstance()
+
+        btnRegister.setOnClickListener({
+            if(!etEmail.text.toString().isEmpty() && !etPassword.text.toString().isEmpty()) {
+                Log.d(TAG,"email: " + etEmail.text + " password : "+ etPassword.text)
+                firebaseAuth.createUserWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                var currentUser = firebaseAuth.currentUser
+                                Log.d(TAG," mail: " + currentUser.toString())
+                            }else {
+                                Log.d(TAG," mail: " + it.exception.toString())
+                            }
+                        }
+                        .addOnFailureListener {
+                            Log.d(TAG," mail: " + it.message)
+                        }
+            }
+        })
 
         tvAlreadyRegistered.setOnClickListener({
             val intent = Intent(this,LogingActivity::class.java)
